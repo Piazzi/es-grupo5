b92f134bb63ff93703434bdb0dd0c8e7db4139e5
FelipeTerrana <felipetc_7@hotmail.com>
2019-04-27 15:11:05 -0300

    Adicionar opção de login e atributo de tipo de usuário

---
+++ header.php
@@ -0,0 +1,71 @@
+<!--Menu-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!--Links do Menu-->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+        <li class="nav-item">
+            <a class="nav-link" href="produto.php">Produtos</a>
+        </li>
+
+
+        <?php
+
+        session_start();
+        if( !isset($_SESSION['islogged']) || !$_SESSION['islogged'] ) { // Nenhum usuário logado
+
+            ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="cadastroUsuario.php">Cadastro de Usuário</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <?php
+        }
+        else
+        {
+            /*
+            include_once("conexao.php");
+
+            $sql = "SELECT * FROM `usuarios` WHERE `email` = " . $_SESSION['email'];
+            $salvar = mysqli_query($conexao,$sql);*/
+
+            if ($_SESSION['tipo'] == "admin") // Só pode cadastrar usuário se não estiver logado OU se for admin
+            {
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="cadastroUsuario.php">Cadastro de Usuário</a>
+                </li>
+                <?php
+            }
+
+            ?>
+
+            <form action="" method="POST" target="_self">
+                <input type="hidden" value="t">
+                <button type="submit" class="btn btn-primary" value="Submit" name="submit">Sair</button>
+            </form>
+
+            <?php
+
+            if( $_POST )
+            {
+                $_SESSION['islogged'] = false;
+                ?>
+                <meta http-equiv="refresh" content="0; url=index.php">
+                <?php
+            }
+        }
+        ?>
+    </ul>
+  </div>
+  <!--Links do Menu-->
+</nav>
+<!--Menu-->
---
+++ login.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cadastro de Usuário</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="publico/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="publico/css/estilo.css">
+</head>
+
+<?php
+include "header.php";
+?>
+
+<hr>
+
+<body>
+<!-- Script para fazer a máscara. Com ele, você pode definir qualquer tipo de máscara com o comando onkeypress="mascara(this, '###.###.###-##')". -->
+<script language="JavaScript">
+    function mascara(t, mask)
+    {
+        var i = t.value.length;
+        var saida = mask.substring(1,0);
+        var texto = mask.substring(i)
+        if (texto.substring(0,1) != saida)
+        {
+            t.value += texto.substring(0,1);
+        }
+    }
+</script>
+<!-- Fim do script -->
+<!-- Formulário de Cadastro de Usuário -->
+<form action="" method="POST" target="_self">
+    <fieldset>
+        <legend>Login</legend>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="name" name="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Senha</label>
+                <input type="password" name="senha" class="form-control" id="inputPassword4" placeholder="Senha">
+            </div>
+        </div>
+    </fieldset>
+    <button type="submit" class="btn btn-primary" value="Submit" name="submit">Confirmar</button>
+</form>
+<!-- Fim do Formulário de Cadastro de Usuário  -->
+<?php
+/* Ligação com Banco de Dados */
+if(isset($_POST["submit"]))
+{
+    include_once("conexao.php");/* Estabelece a conexão */
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM `usuarios` WHERE `email` = '$email' AND `senha`= '$senha'";
+    $salvar = mysqli_query($conexao,$sql);
+
+    if( !isset($_SESSION) )
+        session_start();
+
+    if( mysqli_num_rows($salvar) > 0 )
+    {
+        $_SESSION['islogged'] = true;
+        $_SESSION['email'] = $email;
+        $_SESSION['tipo'] = mysqli_fetch_assoc($salvar)['tipo'];
+        ?>
+        <meta http-equiv="refresh" content="0; url=index.php">
+        <?php
+    }
+    else
+    {
+        ?>
+        <div class="alert alert-warning">Falha ao logar usuário!</div>
+        <?php
+    }
+
+    mysqli_close($conexao);
+
+}
+
+?>
+
+</body>
+
+<hr>
+
+<?php
+include "footer.php";
+?>
+
+</html>
